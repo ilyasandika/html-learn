1881bcf7bf2be2eba5fe5fe725637cbe0b89c442
Ilyas Andika <andikailyas01@gmail.com>
2023-07-28 22:37:02 +0700

    add table in profile modal, add audio

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ const talk = document.getElementById("talk");
 const talkForm = document.getElementById("talkForm");
 const headerDialog = document.getElementById("headerDialog");
 const iframe = document.getElementById("iframe");
+const firstProfile = document.getElementById("firstProfile");
+const secondProfile = document.getElementById("secondProfile");
+const thirdProfile = document.getElementById("thirdProfile");
+const profile = document.getElementById("profile");
+const tableProfile = document.getElementById("tableProfile");
+const fullName = document.getElementById("fullName");
+const nim = document.getElementById("nim");
+const address = document.getElementById("address");
+const audioBtn = document.getElementById("audioBtn");
+const audio = document.getElementById("audio");
 
 structureBtn.addEventListener("click", function () {
     dialog.showModal();
@@ -55,4 +65,36 @@ talkBtn.addEventListener("click", function () {
 talkForm.addEventListener("submit", function (e) {
     e.preventDefault();
     window.location.href = "/index.html";
+});
+
+firstProfile.addEventListener("click", function () {
+    profile.showModal();
+    fullName.innerHTML = "Ilyas Andika";
+    nim.innerHTML = "220401070205";
+    address.innerHTML = "Indonesia";
+});
+
+secondProfile.addEventListener("click", function () {
+    profile.showModal();
+    fullName.innerHTML = "Triani Purnama Sari";
+    nim.innerHTML = "220401070206";
+    address.innerHTML = "Indonesia";
+});
+
+thirdProfile.addEventListener("click", function () {
+    profile.showModal();
+    fullName.innerHTML = "Aan Fadillah Riwahuda";
+    nim.innerHTML = "220401010058";
+    address.innerHTML = "Japan";
+});
+
+audioBtn.addEventListener("click", function () {
+    let attribute = audioBtn.getAttribute("src");
+    if (attribute == "img/play.png") {
+        audioBtn.setAttribute("src", "img/pause.png");
+        audio.play();
+    } else {
+        audioBtn.setAttribute("src", "img/play.png");
+        audio.pause();
+    }
 });
